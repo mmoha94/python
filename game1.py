0c416d335df50e718e0984f4cac388a868f4be80
M Mohammadi <78680631+mmoha94@users.noreply.github.com>
2021-02-07 12:36:41 +0330

    Guess the number
    بازی حدس عدد

---
+++ game1.py
@@ -0,0 +1,18 @@
+import random
+def guss(x):
+   rand_num = random.randint( 1 , x )
+   addad='0'
+   k = 0
+   while int(addad) !=rand_num :
+       k = k + 1
+       addad=input('Enter Number:')
+       
+       if  int(addad) < rand_num :
+            print ('less than...')
+       else: 
+            print  ('greater than ...')  
+ 
+   print ('You Winned after step.')
+
+
+guss(10)
